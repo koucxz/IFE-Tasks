0116f7ab7314705fc357ea746ec6603bc24f9ad8
Sendy Kou <koucxz@hotmail.com>
2017-03-13 22:04:46 +0800

    基础JavaScript练习

---
+++ Nuomi/RegExp/regexp.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Administrator on 2017/3/13.
+ */
+// 判断给定数字是否为手机号码
+function isTel(num) {
+    var reg = /1[34578]\d{9}$/;
+
+    return reg.test(num);
+}
+
+// 18812011232  // 测试结果应该为 true
+// 18812312     // false
+// 12345678909  // false
+
+// 判断输入的字符串是否有相邻重复单词
+function isRepeat(str) {
+    var reg = /\b(\w+)\b\s+\1\b/;
+
+    return reg.test(str);
+}
+
+
+
+var telInput =document.getElementById('telInput');
+var telResult =document.getElementById('telResult');
+
+var strInput =document.getElementById('strInput');
+var strResult =document.getElementById('strResult');
+
+
+
+function addEvent(dom,type,fn){
+    if(dom.addEventListener){
+        dom.addEventListener(type,fn,false);
+    }else if(dom.attachEvent){
+        dom.attachEvent('on'+type,fn);
+    }else{
+        dom['on'+type]=fn;
+    }
+}
+
+addEvent(telInput,'change',function () {
+    var telVal = telInput.value;
+
+    telResult.innerHTML = isTel(telVal) ? 'True':'False';
+})
+
+addEvent(strInput,'change',function () {
+    var strVal = strInput.value;
+    console.log(strVal);
+    strResult.innerHTML = isRepeat(strVal) ? 'True':'False';
+});
