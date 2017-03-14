36464ea31e4f7b7345bbdf0b6cb536c2ec8edef6
Sendy Kou <koucxz@hotmail.com>
2017-03-14 19:39:56 +0800

    正则表达式入门

--- Nuomi/RegExp/regexp.js
+++ Nuomi/RegExp/regexp.js
@@ -1,48 +1,39 @@
 /**
  * Created by Administrator on 2017/3/13.
  */
+var btn = document.getElementsByTagName('button'),
+    telInput =document.getElementById('tel-num'),
+    telResult =document.getElementById('tel-result'),
+    strInput =document.getElementById('str-in'),
+    strResult =document.getElementById('str-result');
+
 // 判断给定数字是否为手机号码
 function isTel(num) {
-    var reg = /1[34578]\d{9}$/;
-
+    var reg = /^1[34578]\d{9}$/;
     return reg.test(num);
 }
 
-// 18812011232  // 测试结果应该为 true
-// 18812312     // false
-// 12345678909  // false
-
 // 判断输入的字符串是否有相邻重复单词
 function isRepeat(str) {
-    var reg = /\b(\w+)\b\s+\1\b/;
-
-    return reg.test(str);
-}
-
-var telInput =document.getElementById('telInput');
-var telResult =document.getElementById('telResult');
-
-var strInput =document.getElementById('strInput');
-var strResult =document.getElementById('strResult');
-
-function addEvent(dom,type,fn){
-    if(dom.addEventListener){
-        dom.addEventListener(type,fn,false);
-    }else if(dom.attachEvent){
-        dom.attachEvent('on'+type,fn);
-    }else{
-        dom['on'+type]=fn;
+    var reg1 = /(\b\w+\b)\s+\1/,
+        reg2 = /(\b\w+\b).+\s+\1/;
+    if (reg1.test(str)) {
+        return '发现相邻重复单词';
+    }
+    else if (reg2.test(str)) {
+        return '有重复单词但是不相邻';
+    }
+    else {
+        return '未发现重复单词';
     }
 }
 
-addEvent(telInput,'change',function () {
+btn[0].onclick = function () {
     var telVal = telInput.value;
-
     telResult.innerHTML = isTel(telVal) ? '输入正确':'请输入一个正确的手机号码';
-});
+};
 
-addEvent(strInput,'change',function () {
+btn[1].onclick = function () {
     var strVal = strInput.value;
-    console.log(strVal);
-    strResult.innerHTML = isRepeat(strVal) ? '发现相邻重复单词':'未发现相邻重复单词';
-});
+    strResult.innerHTML = isRepeat(strVal);
+};
